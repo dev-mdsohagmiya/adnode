a1a795a1502925e6eeb2ef9d8f2120f1aad4ad2b
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-05-11 20:40:22 +0600

    5

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello1");
+const { spawn } = require("child_process");
+
+// Define the coordinates of the tap event
+const x = 500;
+const y = 1000;
+
+// Use the adb tool to send the tap event
+const cmd = spawn("adb", ["shell", "input", "tap", x, y]);
+
+// Log any errors that occur
+cmd.stderr.on("data", (data) => {
+  console.error(`adb error: ${data}`);
+});
+
+// Log the output of the command
+cmd.stdout.on("data", (data) => {
+  console.log(`adb output: ${data}`);
+});
+
+// Log a message when the command completes
+cmd.on("close", (code) => {
+  console.log(`adb command exited with code ${code}`);
+});
